8cba73d38cce1162a5c8e0338865258bc3df550b
PJ <paul@pauljequann.com>
2022-05-12 17:13:34 -0400

    added cart component and functionality

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { ProductsProvider } from './contexts/products.context'
 import { UserProvider } from './contexts/user.context';
+import { CartProvider } from './contexts/cart.context'
 import reportWebVitals from './reportWebVitals';
 
 import './index.scss';
@@ -15,7 +16,9 @@ root.render(
         <BrowserRouter>
             <UserProvider>
                 <ProductsProvider>
-                    <App />
+                    <CartProvider>
+                        <App />
+                    </CartProvider>
                 </ProductsProvider>
             </UserProvider>
         </BrowserRouter>
